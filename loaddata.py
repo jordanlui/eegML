ceb90ab2f55682707a72148b5c9fed30357f626c
Jordan Lui <jordan.lui@gmail.com>
2016-11-30 21:04:19 -0800

    basic init

---
+++ loaddata.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 20:57:15 2016
+
+@author: Jordan
+"""
+
+# EEG Machine Learning Project
+
+# Initialize
+import scipy.io as sio
+
+data = sio.loadmat('../data/s01.mat')
