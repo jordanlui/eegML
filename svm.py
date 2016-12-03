6b20938b0435aa549656154e3ea52d9f8695e7b0
Jordan Lui <jordan.lui@gmail.com>
2016-12-03 11:14:17 -0800

    svm working with up to 70% with 90% train

--- svm.py
+++ svm.py
@@ -10,17 +10,22 @@ Created on Wed Nov 30 20:57:15 2016
 # Initialize
 from __future__ import division
 import scipy.io as sio
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import neighbors, datasets
+#from sklearn import neighbors, datasets
 from sklearn import svm
-import glob, os
-import csv
+#import glob, os
+#import csv
 from matplotlib.colors import ListedColormap
 
 # ML Parameters
 h = .02  # step size in the mesh
 n_neighbors = 15
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 
 # Create color maps
@@ -31,10 +36,14 @@ cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 x = np.genfromtxt('../data/seed/x_de_LDS.csv', delimiter=',')
 y = sio.loadmat('../data/seed/y.mat')
 y=y['y']
-y=y[0:660]
+y=y[0:660] # cut down length of y if our x is shorter
+
+# Shuffle our data
+[x,y] = unison_shuffled_copies(x,y)
 
 # Segment our data
-segment = 10*15*3 # First 10 people
+percent_training = 0.9 # percent of data we want to train on
+segment = int(len(x) * percent_training) # Segment of data that we train on, rounded to an integer 
 x_train = x[:segment,:]
 x_test = x[segment:,:]
 y_train = y[:segment,:]
@@ -51,6 +60,5 @@ testdata = clf.predict(x_test)
 testdata = np.reshape(testdata,(len(testdata),1)) # reshape the data
 compare = testdata==y_test
 numcorrect = np.sum(compare)
-accuracy = numcorrect / len(testdata) * 100 # This is the correct percentage
-
-#plt.show()
+accuracy = numcorrect / len(testdata) * 100 # This is the percentage of correct classifications
+print 'accuracy encountered is ', accuracy ,'%'
