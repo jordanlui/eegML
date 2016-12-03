5aa70e48b003de58d79ddad0aef96f51f08b82a8
Jordan Lui <jordan.lui@gmail.com>
2016-12-03 12:55:41 -0800

    changed to SVM linear. Loading the new 675 trial data

--- svm.py
+++ svm.py
@@ -20,7 +20,7 @@ from matplotlib.colors import ListedColormap
 
 # ML Parameters
 h = .02  # step size in the mesh
-n_neighbors = 15
+#n_neighbors = 15
 
 def unison_shuffled_copies(a, b):
     assert len(a) == len(b)
@@ -33,39 +33,57 @@ cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 # Load our data
-x1 = np.genfromtxt('../data/seed/x_de_LDS.csv', delimiter=',')
-x2 = np.genfromtxt('../data/seed/x_de_movingAve.csv', delimiter=',')
-y = sio.loadmat('../data/seed/y.mat')
-y=y['y']
-y=y[0:660] # cut down length of y if our x is shorter
+x1 = sio.loadmat('../data/seed/processed/de_lds.mat')
+x1 = np.asarray(x1['xmatrix'])
+
+x2 = sio.loadmat('../data/seed/processed/de_movingave.mat')
+x2 = np.asarray(x2['xmatrix'])
+
 
 # Join our matrices
 x3 = np.concatenate((x1,x2),axis=1)
 x = x3
 
+y = sio.loadmat('../data/seed/y.mat')
+y=y['y']
+if len(x) < len(y):
+    y=y[0:len(x)] # cut down length of y if our x is shorter
 
 
 # Shuffle our data
-[x,y] = unison_shuffled_copies(x,y)
 
-# Segment our data
-percent_training = 0.9 # percent of data we want to train on
-segment = int(len(x) * percent_training) # Segment of data that we train on, rounded to an integer 
-x_train = x[:segment,:]
-x_test = x[segment:,:]
-y_train = y[:segment,:]
-y_test = y[segment:,:]
+performance =[]
 
-# Machine Learning
-# Regular Classification
-clf = svm.SVC()
-clf.fit(x_train,y_train)
+for i in range(0,50):
+    [x,y] = unison_shuffled_copies(x,y)
+    
+    # Segment our data
+    percent_training = 0.9 # percent of data we want to train on
+    segment = int(len(x) * percent_training) # Segment of data that we train on, rounded to an integer 
+    x_train = x[:segment,:]
+    x_test = x[segment:,:]
+    y_train = y[:segment,:]
+    y_test = y[segment:,:]
+    
+    # Machine Learning
+    # Regular SVM Classification
+    #clf = svm.SVC()
+    #clf.fit(x_train,y_train)
+    
+    # Try a Linear Kernel SVM
+    
+    
+    lin_clf = svm.LinearSVC(C=2**-4)
+    lin_clf.fit(x_train,y_train)
+    
+    # Test Model
+    testdata = lin_clf.predict(x_test)
+    testdata = np.reshape(testdata,(len(testdata),1)) # reshape the data
+    compare = testdata==y_test
+    numcorrect = np.sum(compare)
+    accuracy = numcorrect / len(testdata) * 100 # This is the percentage of correct classifications
+#    print 'accuracy encountered is ', accuracy ,'%'
+    performance.append(accuracy)
 
-
-# Test Model
-testdata = clf.predict(x_test)
-testdata = np.reshape(testdata,(len(testdata),1)) # reshape the data
-compare = testdata==y_test
-numcorrect = np.sum(compare)
-accuracy = numcorrect / len(testdata) * 100 # This is the percentage of correct classifications
-print 'accuracy encountered is ', accuracy ,'%'
+print 'average accuracy was', np.mean(performance)
+print 'max value was', np.max(performance)
